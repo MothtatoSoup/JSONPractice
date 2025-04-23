eb12a1b146b93546a62501f77177a3ae118659b6
MothtatoSoup <maddy.puryear@outlook.com>
2025-04-23 13:24:55 -0400

    Yippeeeeee

--- dmeo.js
+++ dmeo.js
@@ -1,5 +1,99 @@
 //J S O N
-
 //JSON.stringyfy = coverts a js object to json string
 //json.parse = converts a js string to jso object
 
+
+// Stringafy
+//array
+const names = ["Spongebob", "Patrick", "Squidward", "Sandy"]
+console.log(names)
+
+const jsonStrNms = JSON.stringify(names)
+console.log(jsonStrNms)
+
+//object
+const Spongebob = {
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true,
+    "hobbies": ["Jellyfishing", "Karate", "Cooking"]
+}
+console.log(Spongebob)
+
+const jsonStrSponge = JSON.stringify(Spongebob)
+console.log(jsonStrSponge)
+
+// array of objects
+const bB = [
+    {
+        "name": "Spongebob",
+        "age": 30,
+        "isEmployed": true
+    },
+    {
+        "name": "Patrick",
+        "age": 34,
+        "isEmployed": false
+    },
+    {
+        "name": "Squidward",
+        "age": 50,
+        "isEmployed": true
+    },
+    {
+        "name": "Sandy",
+        "age": 27,
+        "isEmployed": false
+    }
+]
+console.log(bB)
+
+const jsonStrBb = JSON.stringify(bB)
+console.log(bB)
+
+// Parse
+const jsonNames = `["Spongebob", "Patrick", "Squidward", "Sandy"]`
+console.log(jsonNames)
+const jsonParseName = JSON.parse(jsonNames)
+console.log(jsonParseName)
+
+const jsonSB = `{
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true,
+    "hobbies": ["Jellyfishing", "Karate", "Cooking"]
+}`
+console.log(jsonSB)
+const jsonParseSB = JSON.parse(jsonSB)
+console.log(jsonParseSB)
+
+const jsonBB = `[
+    {
+        "name": "Spongebob",
+        "age": 30,
+        "isEmployed": true
+    },
+    {
+        "name": "Patrick",
+        "age": 34,
+        "isEmployed": false
+    },
+    {
+        "name": "Squidward",
+        "age": 50,
+        "isEmployed": true
+    },
+    {
+        "name": "Sandy",
+        "age": 27,
+        "isEmployed": false
+    }
+]`
+console.log(jsonBB)
+const jsonParseBB = JSON.parse(jsonBB)
+console.log(jsonParseBB)
+
+//---------------------------------------------------
+//NO more COPY PASTE!!!
+// fetch()
+fetch("./person.json") //fetches the person.json file
